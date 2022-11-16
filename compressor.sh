4dab6f319302d0abbd1b2790f349dd8b88de0fdf
lof1 <105061690+j0s3l0@users.noreply.github.com>
2022-11-16 12:26:02 -0300

    Add files via upload

---
+++ compressor.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#author:Jose Luis Chacano
+
+ext=$1
+
+if [ -z "$ext" ]
+then
+        echo "Uso: ./compressor.sh [extension]"
+        echo ""
+        echo "ejemplo: ./compressor.sh csv"
+        exit
+else
+        count=$(ls -l *.$ext | awk '{print $NF}' | wc -l)
+
+        for list in $(seq 1 $count)
+        do
+                tar -czvf ./compressed/$(ls -l *.$ext | awk '{print $NF}' | sed -n "$list"p)_$(date +"%Y-%m-%d_%I-%M").tar.gz ./$(ls -l *.$ext | awk '{print $NF}' | sed -n "$list"p)
+        done
+
+        if [ $? -eq 0 ]
+        then
+                rm ./*.$ext
+        fi
+fi
